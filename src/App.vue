95ab67098e0d4456351c00521d368589e38b4cf9
dgrzeg <dgr.wroc@gmail.com>
2022-05-09 12:16:13 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>{{ ads[0].title }}</div>
+</template>
+
+<script lang="ts" setup>
+import Ad from '@/types/Ad'
+import { ref } from '@vue/reactivity'
+
+const ads = ref<Ad[]>([
+  {
+    title: `sprzedam Opla`,
+    location: `blok 12 m.9`,
+    price: 1200,
+    id: `1`,
+  },
+  {
+    title: `proszę nie śmiecić i nie rzucać kiepów`,
+    location: `na klatce w bloku 11`,
+    price: 0,
+    id: `2`,
+  },
+  {
+    title: `kupię miejsce w garażu podziemnym`,
+    location: `blok 12 m.1`,
+    price: 25000,
+    id: `3`,
+  },
+  {
+    title: `tanio ziemniaki od rolnika`,
+    location: `tel 111-111-111`,
+    price: 12,
+    id: `4`,
+  },
+  {
+    title: `kupię Passata`,
+    location: `blok 11 m.1`,
+    price: 9999,
+    id: `5`,
+  },
+])
+</script>
+
+<style lang="scss"></style>
---
+++ src/types/Ad.ts
@@ -0,0 +1,8 @@
+interface Ad {
+  title: string
+  location: string
+  price: number
+  id: string
+}
+
+export default Ad
